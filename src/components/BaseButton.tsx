0b90e689d513c92f411bba57967194ad0b5b88a3
ayodejipy <ay.programmr@gmail.com>
2025-01-18 12:48:06 +0100

    feat: added dashboard and user feature groups , refactor hooks and update paths, commented testcommand out in precommit

---
+++ src/components/BaseButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from './ui/button';
+
+export default function BaseButton() {
+    return <Button>BaseButton</Button>;
+}
---
+++ src/features/onBoarding/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Alert } from '@components/ui/alert';
+
+export default function Test() {
+    return (
+        <div>
+            Test Component
+            {/* <Dashboard /> */}
+            <Alert />
+        </div>
+    );
+}
