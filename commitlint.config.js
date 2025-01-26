500dcded2e2aef37cd25734c65de416dc369c1d3
ayodejipy <ay.programmr@gmail.com>
2025-01-26 18:47:49 +0100

    feat(#1000): update commitlint configuration to enforce lower-case subject and issue prefixes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,18 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    parserPreset: {
+        parserOpts: {
+            issuePrefixes: ['#'],
+        },
+    },
     rules: {
+        'references-empty': [2, 'never'],
         'type-enum': [
             2,
             'always',
             ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
         ],
-        'subject-case': [2, 'always', 'sentence-case'],
-        'header-pattern': [2, 'always', /^(\w+)\(#\d+\): .+$/],
-        'header-match-team': [2, 'always', ['type', 'ticket', 'subject']],
+        'subject-case': [2, 'always', 'lower-case'],
         'subject-empty': [2, 'never'],
     },
 };
